67f0282ce6b97a610f6b81b6ee84cddcf2552a00
onatustun <o@ust.sh>
2025-07-01 16:43:12 +0100

    files recursive

--- flake.nix
+++ flake.nix
@@ -16,11 +16,10 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
 
-      imports = [
-        ./apps.nix
-        ./dev-shell.nix
-        ./packages.nix
-        ./pre-commit-hooks.nix
-      ];
+      imports = let
+        inherit (inputs.nixpkgs.lib.filesystem) listFilesRecursive;
+        inherit (inputs.nixpkgs.lib) filter hasSuffix;
+      in
+        filter (file: !(builtins.elem file [./flake.nix ./shell.nix])) (filter (hasSuffix ".nix") (listFilesRecursive ./.));
     };
 }
