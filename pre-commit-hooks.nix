e9aea72971be77e3722e4a13b188339a83385353
onatustun <o@ust.sh>
2025-07-05 15:20:36 +0100

    node

--- pre-commit-hooks.nix
+++ pre-commit-hooks.nix
@@ -2,7 +2,10 @@
   imports = [inputs.pre-commit-hooks.flakeModule];
 
   perSystem.pre-commit.settings = {
-    excludes = ["flake.lock"];
+    excludes = [
+      "flake.lock"
+      "^node_modules/"
+    ];
 
     hooks = {
       alejandra.enable = true;
